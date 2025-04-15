ccf9ce28846e1a8765edda0649f1ab0545348c3f
Aki47-2005 <akramifrahim2@gmail.com>
2025-04-15 02:23:41 -0700

    NEW CHANGE :

---
+++ project.py
@@ -0,0 +1,27 @@
+#COMPUTER LAB MANAGEMENT SYSTEM
+computer_labs = {
+    "Lab 1": {
+        "capacity": 20,
+        "admin": "Kabuye Hisham",
+        "computers": [],
+        "schedule": []
+    },
+    "Lab 2": {
+        "capacity": 25,
+        "admin": "Ssenoga Akram",
+        "computers": [],
+        "schedule": []
+    },
+    "Lab 3": {
+        "capacity": 30,
+        "admin": "Bushirah Zaid",
+        "computers": [],
+        "schedule": []
+    },
+    "Lab 4": {
+        "capacity": 34,
+        "admin": "Walusimbi Adam",
+        "computers": [],
+        "schedule": []
+    }
+}
