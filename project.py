6a396f5f85b3c08337a45a1ece996cdcbb2c935f
Aki47-2005 <akramifrahim2@gmail.com>
2025-04-15 02:29:11 -0700

    Adding computers to each lab

--- project.py
+++ project.py
@@ -25,3 +25,48 @@ computer_labs = {
         "schedule": []
     }
 }
+def add_computers():
+    computer_brands = ["Dell", "HP", "Lenovo", "Asus", "Acer"]
+    
+    for lab_name in computer_labs:
+        lab = computer_labs[lab_name]
+        for i in range(1, lab["capacity"] + 1):
+            brand = computer_brands[i% len(computer_brands)]
+            
+            if brand == "Dell":
+                ram = "8GB"
+                storage = "500GB HDD"
+                os = "Windows 10"
+                processor = "i5"
+            elif brand == "HP":
+                ram = "16GB"
+                storage = "550GB HDD"
+                os = "Windows 11"
+                processor = "i7"
+            elif brand == "Lenovo":
+                ram = "8GB"
+                storage = "256GB"
+                os = "Windows 11"
+                processor = "i5"
+            elif brand == "Asus":
+                ram = "16GB"
+                storage = "512GB"
+                os = "Windows 11"
+                processor = "i6"
+            else:  
+                ram = "4GB"
+                storage = "500GB HDD"
+                os = "Windows 10"
+                processor = "i3"
+            
+            lab["computers"].append({
+                "number": f"{lab_name}-PC{i:03d}",
+                "brand": brand,
+                "specs": {
+                    "RAM": ram,
+                    "Storage": storage,
+                    "OS": os,
+                    "Processor": processor
+                },
+                "condition": "Good" if i % 5 != 0 else "Fair"
+            })
